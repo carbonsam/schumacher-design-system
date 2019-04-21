f483af7d709768aa6f8067326f7259ee689df9f9
Samuel Schumacher <samschu@live.com>
2019-04-21 16:08:59 -0500

    Add more button styles

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import 'uikit/dist/css/uikit.css';
 
-const Button = ({ children, ...rest }) => (
-  <button className="uk-button uk-button-default" {...rest}>
+const Button = ({ children, style, size, fullWidth, ...rest }) => (
+  <button
+    className={`uk-button uk-button-${style || 'default'} ${size &&
+      'uk-button-' + size} ${fullWidth && 'uk-width-1-1'}`}
+    {...rest}
+  >
     {children}
   </button>
 );
--- src/components/button/index.stories.js
+++ src/components/button/index.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
 
@@ -6,12 +6,63 @@ import Button from './index';
 
 storiesOf('Button', module)
   .addDecorator(centered)
-  .add('with text', () => <Button>A cool button</Button>)
-  .add('with text; disabled', () => <Button disabled>A cool button</Button>)
-  .add('with emoji', () => (
-    <Button>
-      <span role="img" aria-label="so cool">
-        ⚡ Super cool button
-      </span>
-    </Button>
+  .add('default style', () => (
+    <Fragment>
+      <Button size="large">A large button</Button>{' '}
+      <Button>A default button</Button>{' '}
+      <Button size="small">A small button</Button> <br />
+      <Button fullWidth>A full-width button</Button>
+    </Fragment>
+  ))
+  .add('primary style', () => (
+    <Fragment>
+      <Button style="primary" size="large">
+        A large button
+      </Button>{' '}
+      <Button style="primary">A default button</Button>{' '}
+      <Button style="primary" size="small">
+        A small button
+      </Button>{' '}
+      <br />
+      <Button style="primary" fullWidth>
+        A full-width button
+      </Button>
+    </Fragment>
+  ))
+  .add('secondary style', () => (
+    <Fragment>
+      <Button style="secondary" size="large">
+        A large button
+      </Button>{' '}
+      <Button style="secondary">A default button</Button>{' '}
+      <Button style="secondary" size="small">
+        A small button
+      </Button>{' '}
+      <br />
+      <Button style="secondary" fullWidth>
+        A full-width button
+      </Button>
+    </Fragment>
+  ))
+  .add('danger style', () => (
+    <Fragment>
+      <Button style="danger" size="large">
+        A large button
+      </Button>{' '}
+      <Button style="danger">A default button</Button>{' '}
+      <Button style="danger" size="small">
+        A small button
+      </Button>{' '}
+      <br />
+      <Button style="danger" fullWidth>
+        A full-width button
+      </Button>
+    </Fragment>
+  ))
+  .add('other styles', () => (
+    <Fragment>
+      <Button style="text">A text button</Button>
+      <br />
+      <Button style="link">A link button</Button>
+    </Fragment>
   ));
