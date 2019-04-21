2189b10ce08b663b9906feae7f79ac23ea768d0b
Samuel Schumacher <samschu@live.com>
2019-04-21 15:06:45 -0500

    Upgrade and theme Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import theme from './theme';
 
 const stories = require.context('../src/components', true, /\.stories\.js$/);
 
 const loadStories = () => {
   stories.keys().forEach(filename => stories(filename));
 };
+
+addParameters({
+  options: {
+    theme
+  }
+});
 
 configure(loadStories, module);
--- src/components/button/index.stories.js
+++ src/components/button/index.stories.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
 
 import Button from './index';
 
 storiesOf('Button', module)
+  .addDecorator(centered)
   .add('with text', () => <Button>A cool button</Button>)
   .add('with text; disabled', () => <Button disabled>A cool button</Button>)
   .add('with emoji', () => (
